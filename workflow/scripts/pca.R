61ecfc7af3433db81d7db010445e7f30f99ca9ad
Yannic Eising <yeising@students.uni-mainz.de>
2025-11-17 14:44:04 +0100

    feat: add pca variable validation

--- workflow/scripts/pca.R
+++ workflow/scripts/pca.R
@@ -7,8 +7,16 @@ library("DESeq2")
 # load deseq2 data
 dds <- readRDS(snakemake@input[[1]])
 
-# obtain normalized counts
+# compute regularized log-transformed counts for PCA
 counts <- rlog(dds, blind=FALSE)
+
+# Validate that the variable exists in colData
+variable <- snakemake@wildcards[["variable"]]
+if (!variable %in% colnames(colData(dds))) {
+  stop(sprintf("Variable '%s' not found in DESeqDataSet colData. Available variables: %s",
+               variable, paste(colnames(colData(dds)), collapse=", ")))
+}
+
 svg(snakemake@output[[1]])
-plotPCA(counts, intgroup = snakemake@wildcards[["variable"]])
+plotPCA(counts, intgroup = variable)
 dev.off()
